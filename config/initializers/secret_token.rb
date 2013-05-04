b3591d0412b88881612012adb4d57a7e924319e7
Brandon Allan Rupert <brandon.a.rupert@gmail.com>
2013-05-04 15:52:35 -0400

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Newtrial::Application.config.secret_token = 'a517acaa9cbb7c9be520471f0b1c3a30ab8887681d9f4dfda4e9eca4c2ea9641a20b312c772ec6c554f0021c7d374a2821c8a7caa9d2e533ff66911438c762d5'
